468192325bee48b265f2d49fe624bf3c3dd0e344
AprendIgor <107777408+IgorVsFeijo@users.noreply.github.com>
2023-02-22 02:22:49 -0300

    bug da momo resolvido

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ contentInputs.forEach(input => {
         else{
             result.innerHTML = entry.value
         }
-
+        check()
         save(result)
     })
 })
@@ -168,4 +168,36 @@ for (item of arrayElements){
     }
 }
 
-document.title = document.getElementById("name-character").innerText
+if(document.getElementById("img").src.trim == ""){
+    document.getElementById("img").src = "imgs/edit-icon.svg"
+}
+
+let arraySpaces = []
+
+for(item of document.getElementsByClassName("about")[0].children){
+    arraySpaces.push(item)
+}
+
+check()
+
+function check(){
+    console.log("chamou")
+    arraySpaces.forEach(ch => {
+    console.log(ch.innerHTML.trim())
+    if(ch.innerHTML.trim() == ""){
+        switch(ch.id){
+            case "name-character":
+                ch.innerHTML = "NOME DO PERSONAGEM"
+            break
+            case "subtitle":
+                ch.innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis provident officia!"
+            break
+            case "text":
+                ch.innerHTML = "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis neque dignissimos dolore tenetur assumenda ratione nihil velit commodi distinctio unde. Natus perspiciatis possimus quidem deserunt fuga iusto itaque magni totam?</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quia laudantium ducimus dolorem ut libero, architecto tempora omnis sed odit ratione at veniam aspernatur fugit mollitia. Incidunt eum itaque esse.</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci fugit possimus, temporibus excepturi ipsam, autem exercitationem odio soluta officia molestias perferendis omnis, nihil porro maxime incidunt placeat impedit nisi? Perferendis?</p>"
+            break
+            }
+        }
+    })
+}
+
+
